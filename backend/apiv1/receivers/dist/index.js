4a107163370a5b2578aef9ba664fc87c2f3706b8
Ariel Veron <ariel.veron@gmail.com>
2021-07-21 22:41:28 -0300

    configured express, types & boilerplate adjusted receivers_api_v1 license to GPL-3.0-only in package.json

--- backend/apiv1/receivers/dist/index.js
+++ backend/apiv1/receivers/dist/index.js
@@ -1,3 +1,14 @@
 "use strict";
-console.log("Solidar!");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = express_1.default();
+app.get("/", (req, res) => {
+    res.send("Solidar API Receivers V1 online!");
+});
+app.listen(3000, () => {
+    console.log("Solidar API Receivers V1 listening on port 3000!");
+});
 //# sourceMappingURL=index.js.map
--- backend/apiv1/receivers/src/index.ts
+++ backend/apiv1/receivers/src/index.ts
@@ -1,1 +1,11 @@
-console.log("Solidar!");
+import express from 'express';
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Solidar API Receivers V1 online!");
+});
+
+app.listen(3000, () => {
+  console.log("Solidar API Receivers V1 listening on port 3000!");
+});
