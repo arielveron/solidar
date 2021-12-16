86a8eb55a8a3959b059432ca94bb801d66b43239
Ariel Veron <ariel.veron@gmail.com>
2021-12-15 23:28:48 -0300

    backend enabled input classes validation applied validation for CreateHopeInput

--- backend/api/src/hope/create-hope.input.ts
+++ backend/api/src/hope/create-hope.input.ts
@@ -1,10 +1,13 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { MinLength } from 'class-validator';
 
 @InputType()
 export class CreateHopeInput {
+  @MinLength(5)
   @Field()
   subject: string;
 
+  @MinLength(5)
   @Field()
   description: string;
 
