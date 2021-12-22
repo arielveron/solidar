685baf4cee2b47726e83368b8c8b99f634db01e4
Ariel Veron <ariel.veron@gmail.com>
2021-12-22 00:11:59 -0300

    backend connected Config, JwtPassport in Auth Updated config schema to include JWT configs

--- backend/api/src/auth/auth.module.ts
+++ backend/api/src/auth/auth.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { AuthResolver } from './auth.resolver';
 import { AuthService } from './auth.service';
 
 @Module({
+  imports: [
+    PassportModule,
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: configService.get('JWT_EXPIRES_IN') },
+      }),
+    }),
+  ],
   providers: [AuthResolver, AuthService],
 })
 export class AuthModule {}
--- backend/api/src/config.schema.ts
+++ backend/api/src/config.schema.ts
@@ -4,4 +4,6 @@ export const configValidationSchema = Joi.object({
   STAGE: Joi.string().required(),
   API_PORT: Joi.string().required(),
   DB_URL: Joi.string().required(),
+  JWT_SECRET: Joi.string().required(),
+  JWT_EXPIRES_IN: Joi.string().required(),
 });
