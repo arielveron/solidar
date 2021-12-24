940ece7a24524c93ada3657af4638ecd1c812edd
Ariel Veron <ariel.veron@gmail.com>
2021-12-24 11:19:42 -0300

    backend created local strategy & validate user service mock db for users installed types for passport-local

--- backend/api/src/auth/auth.service.ts
+++ backend/api/src/auth/auth.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  private users = [{ username: 'ariel', password: this.createPass('test') }];
+
+  async validateUser(username: string, password: string) {
+    const user = await this.users.find((user) => user.username); // replace with usersService
+
+    const valid = await bcrypt.compare(password, user?.password);
+    if (user && valid) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+
+  createPass(password: string): string {
+    const hashedPass = bcrypt.hashSync(password, 10);
+    return hashedPass;
+  }
+}
