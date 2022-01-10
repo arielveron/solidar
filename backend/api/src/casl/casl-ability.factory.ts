654f4d2a0ee9a52452436505eb40ea15deb16227
Ariel Veron <ariel.veron@gmail.com>
2022-01-09 21:09:52 -0300

    backend fixed order update ability to apply to non admin users

--- backend/api/src/casl/casl-ability.factory.ts
+++ backend/api/src/casl/casl-ability.factory.ts
@@ -28,16 +28,17 @@ export class CaslAbilityFactory {
     if (user.isAdmin) {
       can(Action.Manage, 'all'); // read-write access to everything
     } else {
-      can(Action.Create, Hope); // WARNING! permission reserved to managers of NGOs. Replace after capability created
+      can(Action.Create, Hope); // WARNING! permission reserved to managers of ORGs. Replace after capability created
 
       can(Action.Read, Hope);
+      can(Action.Update, Hope, { authorId: user.id });
+
       cannot(Action.Read, Hope, {
         isPublished: false,
         authorId: { $ne: user.id },
       }).because('Access denied');
     }
 
-    can(Action.Update, Hope, { authorId: user.id });
     cannot(Action.Delete, Hope, { isPublished: true });
 
     return build({
