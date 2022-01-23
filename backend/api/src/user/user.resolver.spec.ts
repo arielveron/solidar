5cfec788133eed332f4b26001763e81ca74a5e48
Ariel Veron <ariel.veron@gmail.com>
2022-01-23 15:37:56 -0300

    backend sucessfully tested create public endpoint User.Resolver

--- backend/api/src/user/user.resolver.spec.ts
+++ backend/api/src/user/user.resolver.spec.ts
@@ -1,21 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CaslModule } from '../casl/casl.module';
 import { OrgService } from '../org/org.service';
+import { CreateUserInput } from './dto/create-user.input';
 import { UserResolver } from './user.resolver';
 import { UserService } from './user.service';
 
 describe('UsersRolver', () => {
   let resolver: UserResolver;
-  const mockUserService = () => ({});
-  const mockOrgService = () => ({});
+  const mockUserService = {
+    createUser: jest.fn((dto) => {
+      const { password, ...result } = dto;
+      return result;
+    }),
+  };
+  const mockOrgService = {};
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [CaslModule],
       providers: [
         UserResolver,
-        { provide: UserService, useFactory: mockUserService },
-        { provide: OrgService, useFactory: mockOrgService },
+        { provide: UserService, useValue: mockUserService },
+        { provide: OrgService, useValue: mockOrgService },
       ],
     }).compile();
 
@@ -24,5 +30,20 @@ describe('UsersRolver', () => {
 
   it('should be defined', () => {
     expect(resolver).toBeDefined();
+  });
+
+  describe('createUser', () => {
+    it('calls UserService.createUser and returns a UserType', async () => {
+      const createUserInput: CreateUserInput = {
+        username: 'ariel',
+        password: '1234',
+        firstName: 'Ariel',
+        lastName: 'Veron',
+      };
+      const { password, ...result } = createUserInput;
+
+      expect(resolver.createUser(createUserInput)).toEqual(result);
+      expect(mockUserService.createUser).toHaveBeenCalled();
+    });
   });
 });
