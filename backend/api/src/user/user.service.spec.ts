09036b300d3c49ae0b6fcfb35dbb24462267be2a
Ariel Veron <ariel.veron@gmail.com>
2022-01-25 01:19:37 -0300

    backend tested getValidUsers for a list of valid & invalid users

--- backend/api/src/user/user.service.spec.ts
+++ backend/api/src/user/user.service.spec.ts
@@ -4,7 +4,28 @@ import { User } from './models/user.entity';
 import { UserService } from './user.service';
 describe('UserService', () => {
   let service: UserService;
-  const mockUserRepository = {};
+  const database = [
+    {
+      id: '1',
+      username: 'ariel',
+    },
+    {
+      id: '2',
+      username: 'eduardo',
+    },
+    {
+      id: '3',
+      username: 'veron',
+    },
+  ];
+
+  const mockUserRepository = {
+    database,
+    findOne: jest.fn((ids) => {
+      const result = database.find((user) => user.id === ids.id);
+      return result;
+    }),
+  };
 
   // more info on testing:
   // https://blog.logrocket.com/unit-testing-nestjs-applications-with-jest/
@@ -34,6 +55,46 @@ describe('UserService', () => {
       await expect(service.getValidUsers(userList)).resolves.toHaveLength(0);
       userList = [];
       await expect(service.getValidUsers(userList)).resolves.toHaveLength(0);
+      userList = {
+        constructor() {
+          return;
+        },
+      };
+      await expect(service.getValidUsers(userList)).resolves.toHaveLength(0);
+    });
+
+    it('should accept a list of a single valid Id and return it as a single valid user', async () => {
+      const userList = ['1'];
+      const expectedUser = [
+        {
+          id: '1',
+          username: 'ariel',
+        },
+      ];
+      await expect(service.getValidUsers(userList)).resolves.toStrictEqual(
+        expectedUser,
+      );
+    });
+
+    it('should accept a list of a mixed valid and invalid Id and return it with only the valid users', async () => {
+      let userList = ['1', '2'];
+      const expectedUser = [
+        {
+          id: '1',
+          username: 'ariel',
+        },
+        {
+          id: '2',
+          username: 'eduardo',
+        },
+      ];
+      await expect(service.getValidUsers(userList)).resolves.toStrictEqual(
+        expectedUser,
+      );
+      userList = ['1', '2', 'invalid'];
+      await expect(service.getValidUsers(userList)).resolves.toStrictEqual(
+        expectedUser,
+      );
     });
   });
 });
