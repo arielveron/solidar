11082d7a0c206f69468c3879f58ea3a9ce05b954
Ariel Veron <ariel.veron@gmail.com>
2022-02-09 22:31:40 -0300

    backend testet Hope.Service.createHope for happy path

--- backend/api/src/hope/hope.service.spec.ts
+++ backend/api/src/hope/hope.service.spec.ts
@@ -2,6 +2,8 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { HopeService } from './hope.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Hope } from './models/hope.entity';
+import { CreateHopeInput } from './dto/create-hope.input';
+import { JwtPayload } from 'src/auth/dto/jwt.payload';
 
 describe('HopeService', () => {
   let service: HopeService;
@@ -26,6 +28,16 @@ describe('HopeService', () => {
     }),
     find: jest.fn(() => {
       return hopesDatabase;
+    }),
+    create: jest.fn((hope) => {
+      return hope;
+    }),
+    save: jest.fn((hope) => {
+      const result: Hope = {
+        ...hope,
+        _id: 'someDatabaseId',
+      };
+      return result;
     }),
   };
 
@@ -57,24 +69,56 @@ describe('HopeService', () => {
         isPublished: true,
       };
 
-      expect(service.getHope('1')).resolves.toEqual(expectedHope);
+      await expect(service.getHope('1')).resolves.toEqual(expectedHope);
     });
 
     it('should call getHope with an invalid id and return an undefined hope', async () => {
       const expectedHope = undefined;
 
-      expect(service.getHope('2')).resolves.toEqual(expectedHope);
-      expect(service.getHope('')).resolves.toEqual(expectedHope);
-      expect(service.getHope(null)).resolves.toEqual(expectedHope);
-      expect(service.getHope(undefined)).resolves.toEqual(expectedHope);
+      await expect(service.getHope('2')).resolves.toEqual(expectedHope);
+      await expect(service.getHope('')).resolves.toEqual(expectedHope);
+      await expect(service.getHope(null)).resolves.toEqual(expectedHope);
+      await expect(service.getHope(undefined)).resolves.toEqual(expectedHope);
     });
   });
 
   describe('getHopes', () => {
-    it('should call getHopes and return all hopes in hopesDatabase', () => {
+    it('should call getHopes and return all hopes in hopesDatabase', async () => {
       const expectedHopes = hopesDatabase;
 
-      expect(service.getHopes()).resolves.toEqual(expectedHopes);
+      await expect(service.getHopes()).resolves.toEqual(expectedHopes);
+    });
+  });
+  // TODO: test getHopes dependent on user asking
+
+  describe('createHope', () => {
+    it('should call createHope with a createHopeInput and return a valid Hope', async () => {
+      const expectedHope: Hope = {
+        _id: expect.any(String),
+        id: expect.any(String),
+        subject: 'Some subject',
+        description: 'Some description',
+        createdAt: expect.any(String),
+        createdBy: '1',
+        forOrg: 'org',
+        isPublished: true,
+      };
+
+      const createHopeInput: CreateHopeInput = {
+        subject: 'Some subject',
+        description: 'Some description',
+        forOrg: 'org',
+      };
+      const user: JwtPayload = {
+        id: '1',
+        username: 'ariel',
+        isAdmin: true,
+        isHopeCreator: true,
+      };
+
+      await expect(service.createHope(createHopeInput, user)).resolves.toEqual(
+        expectedHope,
+      );
     });
   });
 });
