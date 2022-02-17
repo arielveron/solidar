a129d0398cca925b014a4dc635265c0cfcca2ad8
Ariel Veron <ariel.veron@gmail.com>
2022-02-16 22:47:27 -0300

    backend tested Org.Resolver.createOrg for happy path

--- backend/api/src/org/org.resolver.spec.ts
+++ backend/api/src/org/org.resolver.spec.ts
@@ -1,30 +1,49 @@
 import { Test } from '@nestjs/testing';
+import { JwtPayload } from '../auth/dto/jwt.payload';
 import { CaslAbilityFactory } from '../casl/casl-ability.factory';
 import { UserService } from '../user/user.service';
-import { Org } from './models/org.entity';
+import { CreateOrgInput } from './dto/create-org.input';
+import { OrgType } from './models/org.type';
 import { OrgResolver } from './org.resolver';
 import { OrgService } from './org.service';
 
 describe('', () => {
   let resolver: OrgResolver;
 
-  const orgDatabase = [
+  const orgDatabase: OrgType[] = [
     {
-      _id: 'someDBId',
       id: '1',
       orgName: 'Org 1',
       owners: ['1'],
       hopeCreators: [],
       enabled: true,
-      createdAt: '2022-10-01',
-      createdBy: '1',
     },
   ];
 
   const mockOrgService = {
     getOrgs: jest.fn(() => orgDatabase),
+    createOrg: jest.fn(
+      async (createOrgInput: CreateOrgInput, user: JwtPayload) => {
+        const result: OrgType = {
+          id: '1',
+          orgName: createOrgInput.orgName,
+          owners: [...createOrgInput.owners],
+          hopeCreators: [...createOrgInput.hopeCreators],
+          enabled: true,
+        };
+
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const willUsedForCreatedByFied = user;
+
+        return result;
+      },
+    ),
   };
-  const mockUserService = {};
+  const mockUserService = {
+    findOne: jest.fn((user: JwtPayload) => {
+      return user;
+    }),
+  };
   const mockCaslAbilityFactory = {};
 
   beforeEach(async () => {
@@ -45,20 +64,46 @@ describe('', () => {
 
   describe('Orgs', () => {
     it('should call Orgs without arguments and must return all orgs', () => {
-      const expectedOrgs: Org[] = [
+      const expectedOrgs: OrgType[] = [
         {
-          _id: expect.any(String),
           id: '1',
           orgName: 'Org 1',
           owners: ['1'],
           hopeCreators: [],
           enabled: true,
-          createdAt: '2022-10-01',
-          createdBy: '1',
         },
       ];
 
       expect(resolver.orgs()).toEqual(expectedOrgs);
+    });
+  });
+
+  describe('CreateOrg', () => {
+    it('should call createOrg with a valid createOrgInput and authorized user, must return valid new Org', async () => {
+      const expectedOrg: OrgType = {
+        id: '1',
+        orgName: 'Org new',
+        owners: ['1'],
+        hopeCreators: [],
+        enabled: true,
+      };
+
+      const createOrgInput: CreateOrgInput = {
+        orgName: 'Org new',
+        owners: ['1'],
+        hopeCreators: [],
+      };
+
+      const user: JwtPayload = {
+        id: '1',
+        username: 'ariel',
+        isAdmin: false,
+        isHopeCreator: false,
+      };
+
+      await expect(resolver.createOrg(createOrgInput, user)).resolves.toEqual(
+        expectedOrg,
+      );
     });
   });
 });
