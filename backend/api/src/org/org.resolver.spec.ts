a82a7a231487658aaa57cf84cb6b93faa986b51a
Ariel Veron <ariel.veron@gmail.com>
2022-02-16 22:14:08 -0300

    backend tested Org.Resolver.orgs for happy path

--- backend/api/src/org/org.resolver.spec.ts
+++ backend/api/src/org/org.resolver.spec.ts
@@ -1,14 +1,30 @@
 import { Test } from '@nestjs/testing';
 import { CaslAbilityFactory } from '../casl/casl-ability.factory';
 import { UserService } from '../user/user.service';
+import { Org } from './models/org.entity';
 import { OrgResolver } from './org.resolver';
 import { OrgService } from './org.service';
 
 describe('', () => {
   let resolver: OrgResolver;
 
+  const orgDatabase = [
+    {
+      _id: 'someDBId',
+      id: '1',
+      orgName: 'Org 1',
+      owners: ['1'],
+      hopeCreators: [],
+      enabled: true,
+      createdAt: '2022-10-01',
+      createdBy: '1',
+    },
+  ];
+
+  const mockOrgService = {
+    getOrgs: jest.fn(() => orgDatabase),
+  };
   const mockUserService = {};
-  const mockOrgService = {};
   const mockCaslAbilityFactory = {};
 
   beforeEach(async () => {
@@ -25,5 +41,24 @@ describe('', () => {
 
   it('should be defined', async () => {
     expect(resolver).toBeDefined();
+  });
+
+  describe('Orgs', () => {
+    it('should call Orgs without arguments and must return all orgs', () => {
+      const expectedOrgs: Org[] = [
+        {
+          _id: expect.any(String),
+          id: '1',
+          orgName: 'Org 1',
+          owners: ['1'],
+          hopeCreators: [],
+          enabled: true,
+          createdAt: '2022-10-01',
+          createdBy: '1',
+        },
+      ];
+
+      expect(resolver.orgs()).toEqual(expectedOrgs);
+    });
   });
 });
